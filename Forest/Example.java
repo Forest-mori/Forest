f3e302339c025f5990d45a5d44fcd9660941c0e8
yuuuuty <yuto@yutos-computer.local>
2014-07-22 17:25:53 +0900

    GNU!
    /Users/yuto/Desktop/Forest/Forest/Example.java

--- Forest/Example.java
+++ Forest/Example.java
@@ -3,8 +3,8 @@ package forest;
 
 public class Example extends Object
 {
-    public static void main(String args[]){
-        
+    public static void main(String args[])
+    {
         ForestModel aModel = new ForestModel();
         return;
     }
