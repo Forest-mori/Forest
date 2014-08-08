5817b27a009181699ac52b22e7704a4dd270ee89
g1244073 <isobe@suzumu-no-MacBook-Air.local>
2014-08-08 11:31:30 +0900

    スクロール？

---
+++ suzumu/Forest.java
@@ -0,0 +1,258 @@
+package forest;
+
+import java.awt.Point;
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Comparator;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.util.TreeMap;
+
+public class Forest extends Object
+{
+
+    
+	/**
+	 * ノードを番号と一緒に束縛する
+	 */
+	private TreeMap<Integer,Node> nodes;
+
+
+	/**
+	 * 一時的にノードの深さを保存する
+	 */
+	private HashMap<String,Integer> depths;
+
+	/**
+	 * ブランチを束縛する
+	 */
+	private ArrayList<Branch> branches;
+
+	/**
+	 * ルートノードを束縛する
+	 */
+	private ArrayList<Node> roots;
+
+	/**
+	 * テキストのタイプを表す。tree = 0,node = 1, branch = 2
+	 */
+	private int textType;
+
+	/**
+	 * 画面更新用にForestModelを束縛する
+	 */
+	private ForestModel aModel;
+
+
+	/**
+	 * 一番のy座標を持つ変数
+	 */
+	public static int underY;
+
+	/**
+	 * フォレストのコンストラクタ
+	 */
+	public Forest(ForestModel aModel)
+	{
+    this.nodes = new TreeMap<Integer,Node>();
+		this.branches = new ArrayList<Branch>();
+		this.roots = new ArrayList<Node>();
+		this.depths = new HashMap<String,Integer>();
+		this.textType = 5;
+		this.aModel = aModel;
+
+	}
+
+	/**
+	 * ForestModelで取ってきたテキストファイルを読み込む
+	 */
+	public void read(File aFile)
+	{
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(aFile));
+			String str = br.readLine();
+            //System.out.println("txtの読み込み実行");
+			while(str != null){
+				System.out.println(str);
+
+				if(str.equals("trees:"))
+				{
+					this.textType = 0;
+				}
+				else if(str.equals("nodes:"))
+				{
+					this.textType = 1;
+				}
+				else if(str.equals("branches:"))
+				{
+					this.textType = 2;
+				}
+				else if(this.textType == 0)
+				{
+					int depthcount = 0;
+					String nodeName = null;
+
+					String depth[] = str.split("-- ");
+					for(int k = 0; k < depth.length; k++)
+					{
+						if(depth[k].equals("|"))
+						{
+
+							depthcount = depthcount + 1;
+						}
+						else
+						{
+							nodeName = depth[k];
+
+						}
+					}
+
+					this.depths.put(nodeName,depthcount);
+
+
+				}
+
+
+				else if(this.textType == 1 || this.textType == 2)
+				{
+					this.create(str);
+				}
+
+
+				str = br.readLine();
+
+
+			}
+
+			br.close();
+		}catch(FileNotFoundException e){
+			System.out.println(e);
+		}catch(IOException e){
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * readで読み込んだデータを元に木を作る
+	 */
+	public void create(String data)
+	{
+		if(this.textType == 1)
+		{
+
+			String[] node = data.split(", ");
+			Node aNode = new Node(Integer.parseInt(node[0]),node[1],this.depths.get(node[1]));
+			this.nodes.put(Integer.parseInt(node[0]),aNode);
+			if(this.depths.get(node[1]) == 0){
+				this.roots.add(aNode);
+			}
+			//System.out.println("名前＝" + node[1] + "深さ＝" + aNode.getDepth());
+		}
+		else if(this.textType == 2)
+		{
+			String[] branch = data.split(", ");
+			Node parentNode = this.nodes.get(Integer.parseInt(branch[0]));
+			Node childNode = this.nodes.get(Integer.parseInt(branch[1]));
+			parentNode.setChild(childNode);
+			childNode.setParent(parentNode);
+			this.nodes.put(Integer.parseInt(branch[0]), parentNode);
+			this.nodes.put(Integer.parseInt(branch[1]), childNode);
+			Branch aBranch = new Branch(parentNode,childNode);
+			this.branches.add(aBranch);
+		}
+		return;
+	}
+
+	/**
+	 * 探索処理をして、モデルに通知して画面を更新する
+	 */
+	public void visit(Node aNode, Point aPoint)
+	{
+        boolean flag = aModel.getFlag();
+        while(flag)
+        {
+            flag = aModel.getFlag();
+            if(aModel.getFlag() == true){
+                flag = true;
+            }
+        
+                System.out.println("false");
+
+        }
+            
+		if(aNode.getVisit() == false)
+		{
+			aNode.setNodeLocation(aPoint.x,aPoint.y);
+		}
+
+		try
+		{
+                Thread.sleep(100);
+		}
+		catch (InterruptedException anException)
+		{
+			throw new RuntimeException(anException.toString());
+		}
+
+		this.aModel.changed();
+
+		int nodey = aPoint.y;
+		int childcount = 0;
+		for(Node child : aNode.getChildren())
+		{
+			while(nodey <= underY){
+                //16はラベルの縦の長さ
+				nodey += 16;
+			}
+			this.visit(child,new Point(aNode.getName().length()*20 + aPoint.x,nodey));
+			child.setVisit();
+			childcount++;
+
+		}
+		if(nodey > this.underY)
+			this.underY = nodey;
+		if(aNode.getVisit() == false)
+			aNode.setNodeLocation(aPoint.x,(nodey+aPoint.y+16) / 2);
+        if(aNode.getChildren().size() == 1){
+            Node child = aNode.getChildren().get(0);
+            aNode.setNodeLocation(aPoint.x,child.getNodeLocation().y);
+        }
+
+
+	}
+
+	/**
+	 * ノードのゲッター
+	 */
+	public TreeMap<Integer,Node> getNode()
+	{
+		return this.nodes;
+	}
+    
+    
+    
+	/**
+	 * ルートノードのゲッター
+	 */
+	public ArrayList<Node> getRoot()
+	{
+		return this.roots;
+	}
+
+	/**
+	 * ブランチのゲッター
+	 */
+	public ArrayList<Branch> getBranch(){
+		return this.branches;
+	}
+
+    
+
+
+}
+
+
---
+++ suzumu/ForestView.java
@@ -0,0 +1,111 @@
+package forest;
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.border.LineBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.util.TreeMap;
+import java.awt.Point;
+import java.awt.FontMetrics;
+import java.util.ArrayList;
+
+
+public class ForestView extends mvc.View
+{
+
+	/**
+	 * 指定されたモデルとコントローラでMVCを構築するコンストラクタ。
+	 */
+	private JPanel p;
+
+	public ForestView(ForestModel aModel,ForestController aController)
+	{
+		super(aModel,aController);
+		p = new JPanel();
+	}
+
+
+	/**
+	 * 描画を行う。
+	 */
+	public void paintComponent(Graphics aGraphics)
+
+	{
+		//System.out.println("------------------viewによる動作----------------");
+		super.paintComponent(aGraphics);
+		ForestModel aModel = (ForestModel)model;
+		Forest aForest = aModel.getForest();
+		if(aForest == null)
+		{
+			System.out.println("フォレストデータがありません");
+		}
+		else
+		{
+			//System.out.println("フォレストデータを確認");
+
+			if(aModel == null)
+			{
+				//System.out.println("モデルが内包されていません");
+			}
+			int width = this.getWidth();
+			int height = this.getHeight();
+			aGraphics.setColor(Color.black);
+			//System.out.println("nodeを作成してみます");
+			TreeMap<Integer,Node> aNode = aForest.getNode();
+			ArrayList<Branch> aBranch = aForest.getBranch();
+
+			if((aNode == null) && (aBranch == null))
+			{
+			//	System.out.println("フォレストにノードがありません");
+			}
+			else
+			{
+				//System.out.println("フォレストにノードを確認");
+				int i = 0;
+				int widthes = 0;
+				Point aPoint = null;
+				FontMetrics fm = aGraphics.getFontMetrics();
+                
+                int barScore = -aModel.getBar();
+                int barScore2 = -aModel.getBar2();
+
+
+
+				for(Node aTest : aNode.values())
+				{
+					//System.out.println("----名前：" + aTest.getName() + "["+i+"]番目----");
+				   // System.out.println("名前["+aTest.getName()+"]の位置座標:"+aTest.getNodeLocation());
+					widthes = fm.stringWidth(aTest.getName());
+
+					//System.out.println("今回の文字のサイズ["+widthes+"]");
+					//ノードのサークルを作成する。(文字入り)
+					aPoint = aTest.getNodeLocation();
+					aGraphics.setColor(Color.white);
+					aGraphics.drawRect(aPoint.x+barScore2,aPoint.y+barScore,widthes+2,16);
+					aGraphics.drawString(aTest.getName(),aPoint.x+2+barScore2,aPoint.y+13+barScore);
+
+					i++;
+				}
+				for (Branch aTest : aBranch)
+				{
+					widthes = fm.stringWidth(aTest.getParent().getName());
+					Point parent = aTest.getParent().getNodeLocation();
+					Point child = aTest.getChild().getNodeLocation();
+					aGraphics.drawLine(parent.x+widthes+2+barScore2,parent.y+8+barScore,child.x+barScore2,child.y+8+barScore);
+				}
+			}
+
+		}
+		//System.out.println("------------------viewによる動作終了----------------");
+
+		return;
+
+	}
+
+
+}
